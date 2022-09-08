b89ac2bb22f9640c502f30fef78100abff83189a
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-09-08 12:04:12 +0300

    Add code of task 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(evt) {
+  let timePlayStop = evt.seconds;
+  console.log(timePlayStop);
+
+  localStorage.setItem('videoplayer-current-time', timePlayStop);
+}
+
+const saveTimeV = localStorage.getItem('videoplayer-current-time');
+
+if (saveTime) {
+  player.setCurrentTime(saveTimeV);
+}
 
