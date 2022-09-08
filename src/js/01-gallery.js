0307ce08f901da7be1988e992fd4c58c37c1f616
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-09-08 11:24:48 +0300

    Add code of task 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+//Створення розмітки
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+//Додавання підписів до зображень з атрибута alt 
+//(Підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення)
+
+new SimpleLightbox('.gallery a', {
+	animationSpeed: 150,
+	captionsData: 'alt',
+	captionDelay: 250,
+});
 
 console.log(galleryItems);
